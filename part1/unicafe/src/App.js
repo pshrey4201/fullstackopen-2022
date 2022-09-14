b20a8e99d98c5751979bab0c788bd070d1bc3169
Shrey Patel <patel.3705@osu.edu>
2022-09-14 13:21:45 -0400

    Completed Exercise 1.6

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const Heading = ( {text} ) => <h1>{text}</h1>
+const Paragraph = ( {text} ) => <p>{text}</p>
+
+const Button = ({ text, handleClick }) => <button onClick={handleClick}>{text}</button>
+
+const Statistics = ({ good, bad, neutral} ) => {
+
+  return (
+    <>
+      <Heading text="Statistics" />
+      <Paragraph text={"good " + good} />
+      <Paragraph text={"neutral " + neutral} />
+      <Paragraph text={"bad " + bad} />
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Heading text="give feedback" />
+      <Button text="good" handleClick={() => setGood(good + 1)} />
+      <Button text="neutral" handleClick={() => setNeutral(neutral + 1)} />
+      <Button text="bad" handleClick={() => setBad(bad + 1)} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
+    </div>
+  )
+}
+
+export default App
