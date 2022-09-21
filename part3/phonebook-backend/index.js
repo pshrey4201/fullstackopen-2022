367ed2d790c8dc32ee2d413f1ec807366cdd18e0
Shrey Patel <patel.3705@osu.edu>
2022-09-21 02:03:51 -0400

    Completed exercise 3.13

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
-
+const Person = require('./models/person')
 morgan.token('data', function getData (req) {
   if(req.method === 'POST') return JSON.stringify(req.body)
 
@@ -14,29 +15,6 @@ app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :data'))
 
-let persons = [
-  {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
-  },
-  {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
-  },
-  {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
-  },
-  {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
-
 app.get('/info', (request, response) => {
   response.send(`
       <p>Phonebook has info for ${persons.length} people</p>
@@ -45,18 +23,17 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-
-  if (person) {
+  Person.findById(request.params.id).then(person => {
     response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  })
+    //response.status(404).end()
 })
 
 app.delete('/api/persons/:id', (request, response) => {
