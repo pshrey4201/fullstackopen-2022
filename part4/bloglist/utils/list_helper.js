7c0846ee945ee0fe6b8b36f882e5bb80db970ad7
Shrey Patel <patel.3705@osu.edu>
2023-01-19 23:09:01 -0500

    Completed exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  let sum = 0
+  blogs.forEach(element => {
+    sum += element.likes
+  })
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
